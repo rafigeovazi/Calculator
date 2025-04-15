6b1b21be4d76411bbad48d7de7fd80bf401240a7
Geosling <145568712+rafigeovazi@users.noreply.github.com>
2025-04-15 22:45:58 +0700

    Update BestCalculatorEver.py

--- BestCalculatorEver.py
+++ BestCalculatorEver.py
@@ -1,3 +1,3 @@
+#I told u nigga, this is the best calculator ever. Only use 2 line of code and you can do a calculation that your brain can't do🤡
 import ast
-
 while True: print(eval(input(">>>")))
